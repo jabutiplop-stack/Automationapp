75aa9ff285351904728d1ce88e87bd30085319a9
jabuti <jabutipl.op@gmail.com>
2025-08-20 14:01:44 +0200

    poprawki

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+// Główny plik startowy aplikacji
+
+// Ładuje zmienne środowiskowe z pliku .env
+import 'dotenv/config';
+
+// Importuje i uruchamia serwer z katalogu src
+import './src/server.js';
